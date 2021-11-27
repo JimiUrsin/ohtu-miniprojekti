e1ca1f1889e8b1144c137e00c133a0206aa49a39
Siiri <siiri.i.hirvonen@gmail.com>
2021-11-27 17:51:12 +0200

    Created data folder and setup a database using sqlite3

---
+++ src/database_setup.py
@@ -0,0 +1,26 @@
+from database_connection import get_database_connection
+
+class DataBase:
+    def create_tables(self, connection):
+        cursor = connection.cursor()
+        cursor.execute("""CREATE TABLE Recommendations
+                        (id INTEGER PRIMARY KEY,
+                        title TEXT,
+                        type TEXT
+                        );""")
+        return True
+
+    def drop_tables(self,connection):
+        cursor = connection.cursor()
+        cursor.execute("DROP TABLE IF EXISTS Recommendations;")
+        return True
+
+    def initialize_database(self):
+        connection = get_database_connection()
+        connection.isolation_level = None
+
+        self.drop_tables(connection)
+        self.create_tables(connection)
+
+db = DataBase()
+db.initialize_database()
