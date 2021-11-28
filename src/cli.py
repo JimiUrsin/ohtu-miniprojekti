23ca41ef75b6a5e570eda9d7536c9202bf867fd0
Emilia Andersson <emilia.andersson@helsinki.fi>
2021-11-28 21:09:30 +0200

    Fix bugs in CLI and add a test

--- src/cli.py
+++ src/cli.py
@@ -5,18 +5,25 @@ class CLI:
 
     def start(self):
         print('Welcome! Choose an action: ')
-        while(True):
+        continue_loop = True
+        while(continue_loop):
             action = input('1: Add a recommendation, 0: Quit ')
             if action=='0':
                 break
             if action=='1':
-                self.add_new()
+                continue_loop = self.add_new()
 
     def add_new(self):
         while(True):
             title = input('Title of the item: ')
-            check = input(f'Add {title} to collection? 1: Yes, 0: No, cancel ')
-            if check=='0':
-                break
-            if check=='1':
-                self.repository.add(title)
+            while(True):
+                check = input(f'Add "{title}" to collection? 1: Yes, 2: No, reinput title, 0: Quit ')
+                if check=='1':
+                    if self.repository: #tää ehto on väliaikasesti testejä varten
+                        self.repository.add(title)
+                    print(f'"{title}" was added!')
+                    return True
+                if check=='2':
+                    break
+                if check=='0':
+                    return False
---
+++ src/tests/test_cli.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import patch, call
+from cli import CLI
+
+class TestCLI(unittest.TestCase):
+
+    def setUp(self):
+        self.UI = CLI(None)
+
+    @patch('builtins.input', side_effect = ['1', 'Book Name', '1', '0'])
+    @patch('builtins.print')
+    def test_title_read_correctly(self, mock_input, mock_print):
+        self.UI.start()
+        mock_print.assert_has_calls([call('Add "Book Name" to collection? 1: Yes, 2: No, reinput title, 0: Quit ')])
