88396089e205fb29c45eb19f9ffc8bb829277e99
JimiUrsin <jimi.ursin@gmail.com>
2021-12-04 12:25:22 +0200

    Add Video subclass and tests for it

---
+++ src/entities/video.py
@@ -0,0 +1,21 @@
+from entities.recommendation import Recommendation
+
+class Video(Recommendation):
+    """Class that describes an individual video recommendation.
+
+    Attributes:
+        title: A string value that describes the title of the video.
+        type:  A string value that describes the recommendation's type.
+        author: A string value that describes the author of the video.
+        url: A string value that describes the web address of the video.
+    """
+
+    def __init__(self, title, author, url):
+        """Constructor that creates a new video recommendation"""
+
+        super().__init__(title, "Video")
+        self.author = author
+        self.url = url
+
+    def __str__(self):
+        return super().__str__() + f", Author: {self.author}, URL: {self.url}"
---
+++ src/tests/entities/video_test.py
@@ -0,0 +1,17 @@
+from entities.video import Video
+import unittest
+
+class TestVideo(unittest.TestCase):
+    def setUp(self):
+        self.author = "Richard Paul A."
+        self.url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        self.music_video = Video("Not Ever in The Mindset of Handing You Away", self.author, self.url)
+
+    def test_video_author_is_correct(self):
+        self.assertEqual(self.music_video.author, self.author)
+    
+    def test_video_url_is_correct(self):
+        self.assertEqual(self.music_video.url, self.url)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.music_video), "Not Ever in The Mindset of Handing You Away (Video), Author: Richard Paul A., URL: https://www.youtube.com/watch?v=dQw4w9WgXcQ")
