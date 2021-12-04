390c051c0b0d26830a3099cff524c26135cbc394
JimiUrsin <jimi.ursin@gmail.com>
2021-12-04 12:24:35 +0200

    Add Book subclass and tests for it

---
+++ src/tests/entities/book_test.py
@@ -0,0 +1,13 @@
+from entities.book import Book
+import unittest
+
+class TestBook(unittest.TestCase):
+    def setUp(self):
+        self.author = "J.K. Rowling"
+        self.harry_potter = Book("Harry Potter and The Chamber of Secrets", self.author)
+
+    def test_book_author_is_correct(self):
+        self.assertEqual(self.harry_potter.author, self.author)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.harry_potter), "Harry Potter and The Chamber of Secrets (Book), Author: J.K. Rowling")
