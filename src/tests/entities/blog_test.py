701cc129d47fc2ac542e9e3b25da0f324861287c
JimiUrsin <jimi.ursin@gmail.com>
2021-12-04 12:24:09 +0200

    Add Blog subclass and tests for it

---
+++ src/tests/entities/blog_test.py
@@ -0,0 +1,17 @@
+from entities.blog import Blog
+import unittest
+
+class TestBlog(unittest.TestCase):
+    def setUp(self):
+        self.author = "Google"
+        self.url = "https://blog.google/"
+        self.blog = Blog("The Keyword", self.author, self.url)
+
+    def test_blog_author_is_correct(self):
+        self.assertEqual(self.blog.author, self.author)
+    
+    def test_blog_url_is_correct(self):
+        self.assertEqual(self.blog.url, self.url)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.blog), "The Keyword (Blog), Author: Google, URL: https://blog.google/")
