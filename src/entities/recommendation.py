61948a61143969081222e5157b156b2c8390f436
Pekka <75335748+P3Xu@users.noreply.github.com>
2021-12-07 22:21:33 +0200

    Added database id field to recommendation object

--- src/entities/recommendation.py
+++ src/entities/recommendation.py
@@ -6,12 +6,12 @@ class Recommendation:
         type:  A string value that describes what type of a recommendation is.
         """
 
-
-    def __init__(self, title, recom_type):
+    def __init__(self, title, recom_type, db_id):
         """Constructor that creates a new recommendation"""
 
         self.title = title
         self.type = recom_type
+        self.db_id = db_id
 
     def __str__ (self):
         return f"{self.title} ({self.type})"
