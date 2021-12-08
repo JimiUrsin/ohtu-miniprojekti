80fac35510ec402432477056d389c484f4a27452
JimiUrsin <jimi.ursin@gmail.com>
2021-12-08 21:48:27 +0200

    Fix pylint errors in main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
-from UI.cli import CLI
-from entities.recommendation import Recommendation
-from services.recommendations_service import RecommendationService
 from repositories.recommendation_repository import RecommendationRepository
 from services.console_io_service import ConsoleIOService
+from services.recommendations_service import RecommendationService
+from UI.cli import CLI
 
 
 def main():
+    """Construct and run the application's command line interface"""
     recommendation_repository = RecommendationRepository()
     recommendation_service = RecommendationService(recommendation_repository)
     console_io_service = ConsoleIOService()
-    UI = CLI(recommendation_service, console_io_service)
-    UI.start()
+    cli = CLI(recommendation_service, console_io_service)
+    cli.start()
 
 
 if __name__ == '__main__':
