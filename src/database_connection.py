29ad00b18d26a02d7bdfcf48be1af9ad7d724085
JimiUrsin <jimi.ursin@gmail.com>
2021-12-08 21:35:44 +0200

    Fix pylint errors for database_connection.py

--- src/database_connection.py
+++ src/database_connection.py
@@ -4,11 +4,13 @@ import sqlite3
 dirname = os.path.dirname(__file__)
 
 def get_database_connection():
+    """Connects to the main database and returns the Connection object"""
     connection = sqlite3.connect(os.path.join(dirname, "..", "data", "database.sqlite"))
     connection.row_factory = sqlite3.Row
     return connection
 
 def get_test_database_connection():
+    """Connects to the test database and returns the Connection object"""
     connection = sqlite3.connect(os.path.join(dirname, "..", "data", "test_database.sqlite"))
     connection.row_factory = sqlite3.Row
     return connection
