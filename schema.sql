8fa3c770e4aa8e127c65d03112db4011ba317fb0
Juan Covarrubias <me@juan.fi>
2021-12-10 17:02:40 +0200

    Edited SQL schema to take advantage of new fields

--- schema.sql
+++ schema.sql
@@ -1,14 +1,17 @@
 CREATE TABLE if not exists Recommendations
 (
     id INTEGER PRIMARY KEY,
-    title TEXT,
-    type TEXT,
-    url TEXT
+    title TEXT NOT NULL,
+    author_id INTEGER REFERENCES Authors(id) NOT NULL,
+    type TEXT NOT NULL,
+    url TEXT,
+    isbn TEXT,
+    description TEXT,
+    comment TEXT
 );
 
 CREATE TABLE if not exists Authors
 (
     id INTEGER PRIMARY KEY,
-    recom_id INTEGER REFERENCES Recommendations(id),
-    author TEXT
+    author TEXT NOT NULL
 )
