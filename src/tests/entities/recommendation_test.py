1c39ca740ccf36d0e827e114f8f4063d30bd1dc3
Emilia Andersson <emilia.andersson@helsinki.fi>
2021-12-11 19:51:44 +0200

    Add new fields to Recommendation class and a test for string method of the class

--- src/tests/entities/recommendation_test.py
+++ src/tests/entities/recommendation_test.py
@@ -5,9 +5,11 @@ class TestRecommendation(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.title = "Merge sort algorithm"
-        cls.recom_type = "Video"
+        cls.recom_type = "video"
         cls.db_id = 0
-        cls.recommendation = Recommendation(cls.title, cls.recom_type, cls.db_id)
+        cls.author = 'Videoperson'
+        cls.url = 'www.video-address.fi'
+        cls.recommendation = Recommendation(cls.title, cls.recom_type, cls.db_id, cls.author, cls.url)
 
     def test_new_recommendation_has_correct_title(self):
         self.assertEqual(self.recommendation.title, self.title)
@@ -17,3 +19,6 @@ class TestRecommendation(unittest.TestCase):
 
     def test_new_recommendation_has_correct_id(self):
         self.assertEqual(self.recommendation.db_id, self.db_id)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.recommendation), "Merge sort algorithm (video), Author: Videoperson, URL: www.video-address.fi")
